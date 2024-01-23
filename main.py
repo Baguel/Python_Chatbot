8c63640980f913b7e9640454daf52f7836693534
Baguel <florentin.ganfon@epitech.eu>
2024-01-23 09:51:11 +0000

    corection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from chatbot import main_loop
 from test import get_texte, recognize_speech, speech_to_text
 
 def detect_default_langage():
-    lang = locale.getdefaultlocale()[0].split('_')[0]
+    lang = locale.getdefaultlocale()[0]
     return lang
 
 def translate_to_other_langage(texte):
@@ -16,6 +16,7 @@ def bot():
 
     for i in range (0, len(li)): 
         print(translate_to_other_langage(li[i]))
+    
 
     Choix = input(translate_to_other_langage("Fait ton choix pour continuer:") + " ")
     if Choix == '1':
